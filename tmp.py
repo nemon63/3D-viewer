f239ee4a79d1347fee990a62f7251bb1249ea3af
nemon63 <124278477+nemon63@users.noreply.github.com>
2024-07-05 11:26:34 +0400

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,7 @@
+clipinfo_str = {'name': 'Take 001', 'range':  [-0.0333333, 3.96667], 'rate': 30.0, 'source_range': [0, 4], 'source_rate': 30.0}
+
+start = clipinfo_str['range'][0]
+end = clipinfo_str['range'][1]
+
+print(start)
+print(end)
